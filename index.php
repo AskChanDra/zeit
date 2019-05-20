2de6a606be4973248466802ccb37722c724159c2
askchandra <cpthapa@gmail.com>
2019-05-20 21:17:27 +1000

    Get Method testing

--- index.php
+++ index.php
@@ -1,2 +1,26 @@
-<?php echo "Hello from PHP on Now 2.0!"; ?>
+<?php
+
+$var = "";
+if($_GET){
+  $var = $_GET['var'];
+} else {
+    $var = "Zeit!";
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Hello World ! : Zeit</title>
+</head>
+<body>
+  <h1>
+  Welcome to <?php echo $var; ?>
+  </h1>
+    
+</body>
+</html>
 
